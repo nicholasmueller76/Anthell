0ad34e4250804b94cc857f4c9fbbb11bf3429c12
jgychow <jgychow@ucdavis.edu>
2023-05-23 15:50:28 -0700

    Can click on an entity and get its gameobject in InputManager.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.Tilemaps;
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private GameObject cameraObject;
-    [SerializeField] private Grid grid;
     [SerializeField] private Tilemap tiles;
     [SerializeField] private GameObject tileHighlight;
     private Vector3 mousePosition;
@@ -29,14 +28,25 @@ public class InputManager : MonoBehaviour
 
         // Get the coordinates of the tile that the cursor is currently hovering over.
         // Will show a highlight on the tile that the cursor is on.
-        // Left click and do something
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseTilePosition = tiles.LocalToCell(mousePosition);
         tileHighlight.transform.position = tiles.GetCellCenterLocal(mouseTilePosition);
-        
+
+        // Left click and do something
+        // Will prioritize entities clicked first, then tile
         if (Input.GetButtonDown("Fire1"))
         {
-            Debug.Log("Clicked on tile at " + mouseTilePosition);
+            // Check if any entities have been clicked on. Note that the entities need a collider to be detected!
+            RaycastHit2D mouseHit = Physics2D.Raycast(mousePosition, Vector2.zero);
+            if (mouseHit.collider != null && mouseHit.collider.gameObject.name != "Tilemap")
+            {
+                Debug.Log("Clicked on: " + mouseHit.collider.gameObject.name);
+            }
+            else
+            {
+                // Else click on the tile highlighted
+                Debug.Log("Clicked on tile at " + mouseTilePosition);
+            }
         }
     }
 }
