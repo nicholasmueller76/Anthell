ad71c26fd9c85b4fc8928b52df9d5a4be2f3623e
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2023-06-13 21:09:03 -0700

    Feat: Custom Wave Generation

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -16,7 +16,15 @@ namespace Anthell
         public IEnumerator StartWave(int waveNumber)
         {
             Debug.Log("Starting wave " + waveNumber);
-            WaveInfo wave = waves[waveNumber];
+            WaveInfo wave = null;
+            if (waveNumber < waves.Length)
+            {
+                wave = waves[waveNumber];
+            }
+            else
+            {
+                wave = GenerateRandomWave(waveNumber);
+            }
             foreach (EnemyDelayPair enemy in wave.enemies)
             {
                 GameObject newEnemyGameObject = null;
@@ -41,6 +49,48 @@ namespace Anthell
                 }
                 newEnemy?.setQueenAnt(queenAnt);
                 newEnemy?.AddTask(new EntityTask(EntityTaskTypes.Attack, queenAnt));
+            }
+        }
+
+        public WaveInfo GenerateRandomWave(int waveNumber)
+        {
+            WaveInfo wave = new WaveInfo();
+            wave.enemies = new EnemyDelayPair[0];
+            float difficulty = 0.0f;
+            float targetDifficulty = TargetWaveDifficulty(waveNumber);
+            for (int i = 0; difficulty < targetDifficulty; i++)
+            {
+                System.Array.Resize(ref wave.enemies, wave.enemies.Length + 1);
+                wave.enemies[i] = new EnemyDelayPair();
+                wave.enemies[i].enemyType = (EnemyTypes)Random.Range(0, 3);
+                wave.enemies[i].delay = Random.Range(0, 25);
+                difficulty += EnemyDifficulty(wave.enemies[i]);
+            }
+            return wave;
+        }
+
+        private float TargetWaveDifficulty(int waveNumber)
+        {
+            return 2 * Mathf.Pow(waveNumber, 1.3f);
+        }
+
+        private float EnemyDifficulty(EnemyDelayPair enemy)
+        {
+            return (0.2f * (30 - enemy.delay) * EnemyDifficultyScale(enemy.enemyType));
+        }
+
+        private float EnemyDifficultyScale(EnemyTypes enemyType)
+        {
+            switch (enemyType)
+            {
+                case EnemyTypes.Beetle:
+                    return 1.0f;
+                case EnemyTypes.Mantis:
+                    return 1.5f;
+                case EnemyTypes.AcidMaggot:
+                    return 2.0f;
+                default:
+                    return 0.0f;
             }
         }
 
